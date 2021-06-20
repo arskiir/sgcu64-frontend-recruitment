9c5320f44438b8fba04a4643efae99abfe678777
Sukrit Banthao <69001162+arskiir@users.noreply.github.com>
2021-06-20 21:09:00 +0700

    added neccessar attributes to the Place lcass

--- Problem #2/Place.py
+++ Problem #2/Place.py
@@ -1,9 +1,11 @@
 class Place:
     def __init__(self, name):
         self.name = name
+        self.checked_in_numbers = []
+        self.count_checked_in = 0
 
     def __str__(self: str) -> str:
-        return ""
+        return self.name
 
     def __repr__(self) -> str:
         return self.__name__
