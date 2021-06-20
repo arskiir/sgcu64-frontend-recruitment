a7fc244990ea4ca87e96509ebf377b1122c07d65
Sukrit Banthao <69001162+arskiir@users.noreply.github.com>
2021-06-20 21:05:15 +0700

    added first draft of the 3 main commands

--- Problem #2/main.py
+++ Problem #2/main.py
@@ -4,6 +4,33 @@ from Place import Place
 def main():
     place_names = ["a", 'b', 'c', 'd', 'e', 'f']
     places = [Place(name) for name in place_names]
+    options = create_options()
+
+
+def check_in():
+    pass
+
+
+def checkout():
+    pass
+
+
+def print_people_count():
+    pass
+
+
+def create_options():
+    return {
+        "1": {
+            "name": "Check in user", "action": check_in,
+        },
+        "2": {
+            "name": "Check out user", "action": checkout,
+        },
+        "3": {
+            "name": "Print people count", "action": print_people_count,
+        },
+    }
 
 
 if __name__ == '__main__':
