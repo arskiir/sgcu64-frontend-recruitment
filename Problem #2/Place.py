e6793f4f99d5e56f118388eb96a011215cedcf60
Sukrit Banthao <69001162+arskiir@users.noreply.github.com>
2021-06-20 21:09:41 +0700

    added Place's check in and check out method

--- Problem #2/Place.py
+++ Problem #2/Place.py
@@ -4,6 +4,14 @@ class Place:
         self.checked_in_numbers = []
         self.count_checked_in = 0
 
+    def check_in(self, number: str):
+        self.checked_in_numbers.append(number)
+        self.count_checked_in += 1
+
+    def checkout(self, number: str):
+        self.checked_in_numbers.remove(number)
+        self.count_checked_in -= 1
+
     def __str__(self: str) -> str:
         return self.name
 
