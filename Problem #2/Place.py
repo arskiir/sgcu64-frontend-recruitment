6b6a0fe3de6a65bfeb6302d861640c69281d07d7
Sukrit Banthao <69001162+arskiir@users.noreply.github.com>
2021-06-20 21:04:00 +0700

    added first draft of Place class

---
+++ Problem #2/Place.py
@@ -0,0 +1,9 @@
+class Place:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self: str) -> str:
+        return ""
+
+    def __repr__(self) -> str:
+        return self.__name__
--- Problem #2/main.py
+++ Problem #2/main.py
@@ -1,5 +1,9 @@
+from Place import Place
+
+
 def main():
     place_names = ["a", 'b', 'c', 'd', 'e', 'f']
+    places = [Place(name) for name in place_names]
 
 
 if __name__ == '__main__':
